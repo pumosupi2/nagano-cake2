3aeb6b32028210499ae1d29941b560018b27a122
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-07-22 06:45:30 +0000

    [Add]配送先追加/編集[Fix]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -3,14 +3,12 @@ class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
     @addresses = Address.all
-  end
-
-  def show
-
+    @address.customer_id = current_customer.id
   end
 
   def  create
-    @address = Address.new(addrss_params)
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     if @address.save
       redirect_to addresses_path
     else
@@ -19,17 +17,29 @@ class Public::AddressesController < ApplicationController
     end
   end
 
-  def update
+  def edit
+   @address = Address.find(params[:id])
+  end
 
+  def update
+   @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+  address = Address.find(params[:id])
+  address.destroy
+  redirect_to addresses_path
   end
+
   private
 
-  def addrss_params
-    params.permit(:delivery_post_code, :delivery_address, :delivery_address_name, :customer_id)
+  def address_params
+    params.require(:address).permit(:delivery_post_code, :delivery_address, :delivery_address_name)
   end
 
 end
