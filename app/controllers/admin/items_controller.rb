2289c1791d31ee1f045921268e9813551986fdb6
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-07-24 03:30:39 +0000

    [Add] サイドバー・検索窓・ページネーション

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page])
     @item = Item.new
     @item_images = Item.all
   end
