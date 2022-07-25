fcbaac8b2bb9fea955a45b0951bbd13b32e7f069
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-07-25 02:59:29 +0000

    [Add]Adminの注文履歴詳細ページ作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,29 @@
 class Admin::OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :update]
+
   
   def show
-
+    
   end
 
   def update
-
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order)
+    else
+      render :show
+    end
   end
   
+  
+  private
+  
+  def  order_params
+    params.require(:order).permit(:status)
+  end
+  
+  
+  
+  def set_order
+    @order = Order.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,8 +38,9 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:show, :update]
-    resources :order_histories, only: [:update]
+    resources :orders, only: [:show, :update] do
+      resources :order_histories, only: [:update]
+    end
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
