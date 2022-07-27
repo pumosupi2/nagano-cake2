2e13b1e940ef7065794d2f60b35904fa0a8a2ce1
EC2 Default User <ec2-user@ip-172-31-6-217.ap-northeast-1.compute.internal>
2022-07-27 05:13:54 +0000

    [Add]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -5,7 +5,7 @@ class Public::SessionsController < Devise::SessionsController
   before_action :customer_state, only: [:create]
   
    def after_sign_out_path_for(resource)
-     flash[:notice] = "ログアウトしました"
+     flash[:notice] = "ログアウトしました。"
      root_path
    end
   
